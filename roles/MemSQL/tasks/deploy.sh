583268685bd4a8e22fa7f80fdaa477945edacdbf
rohith17992 <naidu.rohith82@gmail.com>
2018-12-01 09:16:01 +0000

    Ti

---
+++ roles/MemSQL/tasks/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+memsql-deploy cluster-in-a-box --yes --license BDBjZjNmYmFkOTM1NzRmODBhMzYwOWRiYWEwNDE2MGMzAAAAAAAAAAAAAAIAAAAAAAQwNQIZAN2fPlV0dVZC+dudtvtOAtYzq7u8mUPdSgIYK7vr5AmGd2affCtMEIBYFBZOLHR5opcbAA==
+
